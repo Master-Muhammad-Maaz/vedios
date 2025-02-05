ff725acfa63488b200a4f4813b0e524d184dec0f
Master-Muhammad-Maaz <mohammadmaaz8262@gmail.com>
2025-02-05 09:32:03 +0530

    Add files via upload

---
+++ VideoDisplayAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.video_display_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoDisplayAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideoDisplayAppApplication.class, args);
+	}
+
+}
